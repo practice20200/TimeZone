a9bf85677f4237b7fa9632e9034ba4ca985529b7
practice20200 <wor9hard2018@gmail.com>
2022-05-20 20:53:45 -0700

    initial commit

---
+++ Timezone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Timezone
+//
+//  Created by Apple New on 2022-05-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
